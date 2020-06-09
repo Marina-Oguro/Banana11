2b6954660bc61ba88cdeec90e2f51421ee9aaca5
Yuta Horiuchi <y-horiuchi@cresco.co.jp>
2020-06-09 09:36:06 +0900

    add

---
+++ 05_test/Turn_Test.java
@@ -0,0 +1,31 @@
+package Minipro;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+
+
+public class Turn_Test {
+	
+	@Test//目視で確認
+	public void Test1() {
+		Player p = new Player("先攻", Mark.CIRCLE);
+		Board board = new Board();
+		Turn turn = new Turn(p);
+		turn.execute(board);
+		//turn.showTurn();
+		board.showBoard();
+	}
+
+	
+	@Test//目視で確認
+	public void Test2() {
+		Player p = new Player("後攻", Mark.CROSS);
+		Board board = new Board();
+		Turn turn = new Turn(p);
+		turn.execute(board);
+		//turn.showTurn();
+		board.showBoard();
+	}
+
+}
