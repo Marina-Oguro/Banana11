b57ad05ed8bc794a288781b127ce4b77d190e9f0
Yuta Horiuchi <y-horiuchi@cresco.co.jp>
2020-06-12 09:38:35 +0900

    add

---
+++ 04_implementation/反復2/ソースコード/CardStack.java
@@ -0,0 +1,32 @@
+package Minipro2;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collections;
+import java.util.EmptyStackException;
+
+
+public class CardStack {
+
+	private static int CARDS = 52;
+	private List<Card> cards;
+	
+	public CardStack() {
+		cards = new LinkedList<Card>();
+		Suit[] suits = {Suit.SPADE, Suit.DIA, Suit.HEART, Suit.SPADE};
+		for(int i = 0; i < CARDS; i++) {
+			int number = 1 + (i % Card.MAX);
+			Suit suit = suits[(i / Card.MAX)];
+			cards.add(new Card(number, suit));
+		}
+		Collections.shuffle(cards);
+	}
+	
+	public Card drawCard(){
+		if (cards.size() != 0) {
+			return cards.remove(0);
+		} else {
+			throw new EmptyStackException();
+		}
+	}
+}
---
+++ 04_implementation/反復2/ソースコード/Main.java
@@ -0,0 +1,13 @@
+package Minipro2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		CardStack stack =  new CardStack();
+		CardGame cardGame = new CardGame();
+		cardGame.start(stack);
+
+	}
+
+}
